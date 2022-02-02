b1264c1639a30a5c61bfdc8f5d90c6c999cec5e2
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-02 20:35:01 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center bg-black">
+      <h1 className="text-white">Hello NextJS</h1>
+    </div>
+  );
+};
+
+export default Home;
