e35ee21304f36d4bfaa0def330601bbdb9d31b9c
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-02 21:04:47 +0900

    add--layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { VFC } from "react";
+
+type Props = {
+  children: React.ReactNode;
+  title?: string;
+};
+
+export const Layout: VFC<Props> = (props) => {
+  const { children, title = "Home Page" } = props;
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header>
+        <nav className="bg-gray-800 w-screen flex items-center justify-center gap-10 h-14 text-white">
+          <Link href="/">HOME</Link>
+          <Link href="/blog-page">BLOG</Link>
+          <Link href="/contact-page">CONTACT</Link>
+        </nav>
+      </header>
+      <main className="flex flex-1 justify-center items-center flex-col w-screen">
+        {children}
+      </main>
+    </div>
+  );
+};
